10322a1730e3b2c0956c63a4163f00723684cb78
af223 <aimee.feng24@gmail.com>
2021-07-12 19:38:37 -0400

    Retrieve location based on map click

--- app/src/main/java/com/codepath/travel/fragments/MapFragment.java
+++ app/src/main/java/com/codepath/travel/fragments/MapFragment.java
@@ -2,22 +2,43 @@ package com.codepath.travel.fragments;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.codepath.travel.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
+
 public class MapFragment extends Fragment {
+
+    GoogleMap map;
+    private static final String revGeocodeURL = "https://maps.googleapis.com/maps/api/geocode/json";
+    private static final String TAG = "MapsFragment";
+    private TextView tvLocation;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -32,11 +53,49 @@ public class MapFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
+            map = googleMap;
             LatLng sydney = new LatLng(-34, 151);
             googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+                @Override
+                public void onMapLongClick(LatLng latLng) {
+                    reverseGeocode(latLng);
+                }
+            });
         }
     };
+
+    private void showDestinationAlertDialog(final LatLng latLng, String address) {
+        Toast.makeText(getContext(), latLng.toString(), Toast.LENGTH_SHORT).show();
+        View  messageView = LayoutInflater.from(getContext()).inflate(R.layout.map_message_item, null);
+        tvLocation = messageView.findViewById(R.id.tvLocation);
+        tvLocation.setText(address);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+        alertDialogBuilder.setView(messageView);
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "YES",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        BitmapDescriptor defaultMarker =
+                                BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN);
+                        map.addMarker(new MarkerOptions()
+                                .position(latLng)
+                                .title(latLng.toString())
+                                .icon(defaultMarker));
+                    }
+                });
+        alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "NO",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) { dialog.cancel(); }
+                });
+        alertDialog.show();
+    }
+
 
     @Nullable
     @Override
@@ -54,5 +113,41 @@ public class MapFragment extends Fragment {
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
+    }
+
+    private String formatLatlng(LatLng coords) {
+        return String.valueOf(coords.latitude) + "," + String.valueOf(coords.longitude);
+    }
+
+    private void reverseGeocode(LatLng coords) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("latlng", formatLatlng(coords));
+        params.put("key", getResources().getString(R.string.maps_api_key));
+        params.put("location_type", "APPROXIMATE");
+        //params.put("language", "en");
+        client.get(revGeocodeURL, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG,"Success!");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    String address = results.getJSONObject(0).getString("formatted_address");
+                    Log.i(TAG, address);
+                    showDestinationAlertDialog(coords, address);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "SAD:(", throwable);
+                Log.e(TAG, "statusCode");
+                Log.e(TAG, "response");
+            }
+        });
     }
 }
