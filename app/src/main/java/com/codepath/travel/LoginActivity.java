9a8fdd7e2b554aa033c427dcf785138405306a61
af223 <aimee.feng24@gmail.com>
2021-07-12 13:49:32 -0400

    Incorporate Parse

--- app/src/main/java/com/codepath/travel/LoginActivity.java
+++ app/src/main/java/com/codepath/travel/LoginActivity.java
@@ -3,12 +3,44 @@ package com.codepath.travel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    
+    private EditText etName;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnSignup;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        etName = findViewById(R.id.etName);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+        
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etName.getText().toString();
+                String password = etPassword.getText().toString();
+                if (username.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Need a username", Toast.LENGTH_SHORT).show();
+                } else if (password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Need a password", Toast.LENGTH_SHORT).show();
+                } else {
+                    login(username, password);
+                }
+            }
+        });
+    }
+
+    private void login(String username, String password) {
     }
 }
---
+++ app/src/main/java/com/codepath/travel/ParseApplication.java
@@ -0,0 +1,20 @@
+package com.codepath.travel;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.app_id))
+                .clientKey(getString(R.string.client_key))
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
