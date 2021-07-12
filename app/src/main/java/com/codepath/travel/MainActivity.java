72db93b3b50198aa5b5e0bccbe0b139018ced5f4
af223 <aimee.feng24@gmail.com>
2021-07-12 16:17:11 -0400

    Bottom navigation with GoogleMaps fragment

--- app/src/main/java/com/codepath/travel/MainActivity.java
+++ app/src/main/java/com/codepath/travel/MainActivity.java
@@ -2,20 +2,51 @@ package com.codepath.travel;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.codepath.travel.fragments.MapFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_map:
+                        fragment = new MapFragment();
+                        break;
+                    case R.id.action_locations:
+                        fragment = new MapFragment();
+                        break;
+                    case R.id.action_costs:
+                    default:
+                        fragment = new MapFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_map);
     }
 
     @Override
