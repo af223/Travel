168eb73e1c8fe6ad6916736d4e7c381d4cfab3ed
af223 <aimee.feng24@gmail.com>
2021-07-27 16:48:28 -0400

    Add endless scrolling and progress bar while loading transportation options

--- app/src/main/java/com/codepath/travel/TransportationActivity.java
+++ app/src/main/java/com/codepath/travel/TransportationActivity.java
@@ -2,6 +2,8 @@ package com.codepath.travel;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,21 +35,35 @@ public class TransportationActivity extends AppCompatActivity {
     private static final String YELP_BUSINESS_SEARCH_URL = "https://api.yelp.com/v3/businesses/search";
     private static final String CATEGORIES = "transport,carrental,bikerentals,motorcycle_rental,trainstations";
     private static final Integer NUM_LOAD_BUSINESSES = 25;
+    private static int offset;
+    private ProgressBar progressBarTransportation;
     private RecyclerView rvTransportations;
     private TransportationsAdapter adapter;
     private ArrayList<YelpData> transportations;
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transportation);
 
+        offset = 0;
+        progressBarTransportation = findViewById(R.id.progressBarTransportation);
+        progressBarTransportation.setVisibility(View.VISIBLE);
+
         rvTransportations = findViewById(R.id.rvTransportations);
         transportations = new ArrayList<>();
         adapter = new TransportationsAdapter(this, transportations);
-        rvTransportations.setLayoutManager(new LinearLayoutManager(this));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvTransportations.setLayoutManager(linearLayoutManager);
         rvTransportations.setAdapter(adapter);
-
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                loadTransportation();
+            }
+        };
+        rvTransportations.addOnScrollListener(scrollListener);
         loadTransportation();
     }
 
@@ -59,12 +75,15 @@ public class TransportationActivity extends AppCompatActivity {
         params.put("longitude", getIntent().getStringExtra(Destination.KEY_LONG));
         params.put("categories", CATEGORIES);
         params.put("limit", NUM_LOAD_BUSINESSES);
+        params.put("offset", offset);
         headers.put("Authorization", "Bearer " + getResources().getString(R.string.yelp_api_key));
         client.get(YELP_BUSINESS_SEARCH_URL, headers, params, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 YelpData.processYelpResults(transportations, json.jsonObject, TransportationActivity.this);
                 adapter.notifyDataSetChanged();
+                progressBarTransportation.setVisibility(View.GONE);
+                offset += NUM_LOAD_BUSINESSES;
             }
 
             @Override
